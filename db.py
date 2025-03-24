d22cdf76dc9154bbc623804f1a167d563709e230
crazyquark <cristian.sandu@gmail.com>
2025-03-24 15:20:27 +0200

    update

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import datetime
 from pymongo import MongoClient
 from config.mongo import MONGO_PATH
 
+
 def connect_mongo():
     from urllib.parse import quote_plus
     uri = "mongodb://%s:%s@%s" % (
@@ -11,23 +12,12 @@ def connect_mongo():
 
     return client
 
-def store(data, client  = connect_mongo()):
-    db = client.kraken
-    eth_price_history = db.eth_price_history
-    btc_price_history = db.btc_price_history
 
-    # Price at which the last tx closed
-    last_eth_price = data['XETHZEUR']['c'][0]
-    last_btc_price = data['XXBTZEUR']['c'][0]
+def store(data, client=connect_mongo()):
+    db = client.steam
+    user = data['user']
+    user_db = db[user]
+    print(data)
 
-    eth_id = eth_price_history.insert_one({
-        'price': last_eth_price,
-        'date': datetime.datetime.now(tz=datetime.timezone.utc),
-    }).inserted_id
-    print(eth_id)
-
-    btc_id = btc_price_history.insert_one({
-        'price': last_btc_price,
-        'date': datetime.datetime.now(tz=datetime.timezone.utc),
-    }).inserted_id
-    print(btc_id)
+    del data['user']
+    user_db.insert_one(data)
--- steam-monitor.py
+++ steam-monitor.py
@@ -3,19 +3,61 @@ from steam_web_api import Steam
 
 from zoneinfo import ZoneInfo
 from datetime import datetime
+import asyncio
 
 from config.secrets import STEAM_WEBAPI_KEY
+import db
 
+users = ['crazyquark']
 steam = Steam(STEAM_WEBAPI_KEY)
+wait_time = 60
 
-res = steam.users.search_user('crazyquark')
-print(res)
-logoff = datetime.fromtimestamp(
-    res['player']['lastlogoff'], tz=ZoneInfo('Europe/Bucharest'))
-online = res['player']['personastate']
-gameid = res['player']['gameid'] if 'gameid' in res['player'] else ''
-game = res['player']['gameextrainfo'] if 'gameextrainfo' in res['player'] else ''
-print(logoff)
-print(online)
-print(gameid)
-print(game)
+prev_state = {}
+
+
+def check_status(user):
+    tz = ZoneInfo('Europe/Bucharest')
+    res = steam.users.search_user(user)
+    # print(res)
+    # logoff = datetime.fromtimestamp(
+    #     res['player']['lastlogoff'], tz=tz)
+    # online = int(res['player']['personastate'])
+
+    gameId = res['player']['gameid'] if 'gameid' in res['player'] else ''
+    game = res['player']['gameextrainfo'] if 'gameextrainfo' in res['player'] else ''
+
+    prev_online = prev_state[user][gameId] if user in prev_state and gameId in prev_state[user] else 0
+    if gameId:
+        if prev_online == 0:
+            # Started game
+            start_time = datetime.now(tz)
+            db.store(data={
+                'user': user,
+                'game_id': gameId,
+                'game': game,
+                'start_time': start_time
+            })
+
+        prev_state[user][gameId] = 1
+    elif prev_state == 1:
+        # Game end
+        end_time = datetime.now(tz)
+        db.store(date={
+            'user': user,
+            'game_d': gameId,
+            'game': game,
+            'end_time': end_time,
+        })
+
+        prev_state[user][gameId] = 0
+
+
+async def check_user_loop():
+    while True:
+        for user in users:
+            check_status(user)
+        await asyncio.sleep(wait_time)
+
+
+if __name__ == '__main__':
+    asyncio.run(check_user_loop())

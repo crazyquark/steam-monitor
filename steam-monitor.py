2cc84ca71cf4bd511c81507a30746afa52d0a2ca
crazyquark <cristian.sandu@gmail.com>
2025-03-25 13:27:28 +0200

    add start time

--- steam-monitor.py
+++ steam-monitor.py
@@ -83,6 +83,7 @@ def check_status(user):
             db.store(dbname='session', data={
                 'user': user,
                 'game_id': gameId,
+                'start_time': start_time,
                 'duration': (end_time - start_time).total_seconds()
             })
 
